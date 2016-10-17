d8b1bdf9af6e18201f8735f86b844eed035289e7
marylu <marylo281292@gmail.com>
2016-10-16 21:13:02 -0400

    convertir la instancia leida en estado y agregado la estructura base del dfs

--- Proyecto1/global/dfid.cpp
+++ Proyecto1/global/dfid.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+void DFS (state_t state, int hist, int d) {
+
+    if (d > b) 
+        return;
+
+    // VARIABLES FOR ITERATING THROUGH state's SUCCESSORS
+    state_t child;
+    ruleid_iterator_t iter; // ruleid_terator_t is the type defined by the PSVN API successor/predecessor iterators.
+    int ruleid; // an iterator returns a number identifying a rule
+    int aux;
+
+    // LOOP THOUGH THE CHILDREN ONE BY ONE
+    init_fwd_iter(&iter, &state);  // initialize the child iterator
+    ++ns[d]; 
+    while( (ruleid = next_ruleid(&iter)) >= 0 ) {
+        if (fwd_rule_valid_for_history(hist,ruleid)) {
+            aux = next_fwd_history(hist,ruleid);
+            apply_fwd_rule(ruleid, &state, &child);
+            DFS (child, aux, d+1);
+        }
+
+    }
+
 int main(int argc, char **argv ) {
 
     ifstream in;
@@ -12,10 +35,18 @@ int main(int argc, char **argv ) {
     out.open (argv[2]);
     string line;
 
+    ssize_t nchars; 
+    state_t state;
+
     out << "grupo, algorithm, domain, instance, cost, generated, time, gen_per_sec\n"
 
     if (in.is_open()) {
         while (getline (in,line)) {
+            nchars = read_state(line, &state);
+            if( nchars <= 0 ) {
+                printf("Error: invalid state entered.\n");
+                return 0;
+    }
             out << line << "\n";
         }
     }
