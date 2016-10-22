788f30b35700b0727b16c0caa868c2ad908b45ce
root_ngchiseng <ngchiseng@ubuntu.(none)>
2016-10-21 18:41:48 -0700

    Script de la actividad 3 y 4 modificados.

--- Proyecto1/Actividad3/Script.sh
+++ Proyecto1/Actividad3/Script.sh
@@ -2,13 +2,13 @@
 
 if [ $# != 5 ];then
     echo "./Script <Num_ini> <Num_fin> ./<Program> ../intances/<Arch>.txt <Arch>.csv"
-fi 
-D=$4
-D=${D:13}
-echo "grupo, algorithm, domain, instance, cost, generated, time, gen_per_sec" >> $5
-let _i=1
-while read line
-do 
+else
+    D=$4
+    D=${D:13}
+    echo "grupo, algorithm, domain, instance, cost, generated, time, gen_per_sec" >> $5
+    let _i=1
+    while read line
+    do 
         G="$line"
 	if [ \( $1 -le $_i \) -a \( $2 -ge $_i \) ];then
 		timeout 5m $3 "$G" $D >> $5
@@ -17,5 +17,7 @@ do
 		fi			
 	fi
 	let _i=$_i+1 
-done < $4
+    done < $4
+fi 
+
 
--- Proyecto1/Actividad4/Script.sh
+++ Proyecto1/Actividad4/Script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-if [ $# != 5 ];then
-    echo "./Script <Num_ini> <Num_fin> ./<Program> ../intances/<Arch>.txt <Arch>.csv"
-fi 
-D=$4
-D=${D:13}
-echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> $5
-let _i=1
-while read line
-do 
+if [ $# != 7 ];then
+    echo "./Script <Num_ini> <Num_fin> ./<Program> ../intances/<Arch>.txt <Arch>.csv <Algorithm> <Heurist>"
+else
+    D=$4
+    D=${D:13}
+    echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> $5
+    let _i=1
+    ulimit -m 2048000
+    while read line
+    do
         G="$line"
 	if [ \( $1 -le $_i \) -a \( $2 -ge $_i \) ];then
 		timeout 5m $3 "$G" $D >> $5
 		if [ $? == 124 ];then
-			echo "X, dfid, $D, \"$line\", na, na, na, na" >> $5 # -----> agregar algoritmo y heuristica utilizada 
+			echo "X, $6, $7, $D, \"$line\", na, na, na, na" >> $5 # -----> agregar algoritmo y heuristica utilizada 
 		fi			
 	fi
 	let _i=$_i+1 
-done < $4
+    done < $4 
+fi 
+
+        
+
 
