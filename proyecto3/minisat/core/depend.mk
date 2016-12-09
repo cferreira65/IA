b68e95349fce94b34a23cd7beae116c9c2162b9f
root_ngchiseng <ngchiseng@ubuntu.(none)>
2016-12-08 16:23:20 -0800

    Agregado Minisat.

---
+++ proyecto3/minisat/core/depend.mk
@@ -0,0 +1,27 @@
+/home/ngchiseng/Desktop/minisat/core/Main.o /home/ngchiseng/Desktop/minisat/core/Main.or /home/ngchiseng/Desktop/minisat/core/Main.od /home/ngchiseng/Desktop/minisat/core/Main.op: /home/ngchiseng/Desktop/minisat/core/Main.cc \
+ /home/ngchiseng/Desktop/minisat/utils/System.h \
+ /home/ngchiseng/Desktop/minisat/mtl/IntTypes.h \
+ /home/ngchiseng/Desktop/minisat/utils/ParseUtils.h \
+ /home/ngchiseng/Desktop/minisat/utils/Options.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Vec.h \
+ /home/ngchiseng/Desktop/minisat/mtl/XAlloc.h \
+ /home/ngchiseng/Desktop/minisat/core/Dimacs.h \
+ /home/ngchiseng/Desktop/minisat/core/SolverTypes.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Alg.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Map.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Alloc.h \
+ /home/ngchiseng/Desktop/minisat/core/Solver.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Heap.h
+/home/ngchiseng/Desktop/minisat/core/Solver.o /home/ngchiseng/Desktop/minisat/core/Solver.or /home/ngchiseng/Desktop/minisat/core/Solver.od /home/ngchiseng/Desktop/minisat/core/Solver.op: /home/ngchiseng/Desktop/minisat/core/Solver.cc \
+ /home/ngchiseng/Desktop/minisat/mtl/Sort.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Vec.h \
+ /home/ngchiseng/Desktop/minisat/mtl/IntTypes.h \
+ /home/ngchiseng/Desktop/minisat/mtl/XAlloc.h \
+ /home/ngchiseng/Desktop/minisat/core/Solver.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Heap.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Alg.h \
+ /home/ngchiseng/Desktop/minisat/utils/Options.h \
+ /home/ngchiseng/Desktop/minisat/utils/ParseUtils.h \
+ /home/ngchiseng/Desktop/minisat/core/SolverTypes.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Map.h \
+ /home/ngchiseng/Desktop/minisat/mtl/Alloc.h
