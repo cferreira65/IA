720c665ae2afef0809adb93f3f4e5066b00a6229
NgChiseng <ngchiseng@gmail.com>
2016-12-11 19:09:56 -0800

    Agregado el comando que borra el out.txt al comienzo.

--- proyecto3/Enunciado/Script.sh
+++ proyecto3/Enunciado/Script.sh
@@ -3,12 +3,13 @@
 if [ $# != 2 ];then
     echo "Ingrese:  ./Script <Arch_Input>.txt ./.../PATH/../core/minisat"
 else
+    rm -f out.txt
     make
     while read line
     do  
         L="$line"
         echo $L > arch.txt
-        echo $L >> out.cnf
+        echo $L >> out.txt
         G=${L[0]}
         F=${L[1]}
         ./encode arch.txt
